09ee6e2388ccf53cd91b62bce960e0649bbe2a90
Venen-S <57008160+Venen-S@users.noreply.github.com>
2023-04-17 13:58:40 +0300

    Delete RomanCalculator directory

--- RomanCalculator/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using NumericConverters;
-
-int arabicNum = 3999;
-string romanNum = RomanNumeralConverter.ArabicToRoman(arabicNum);
-Console.WriteLine("{0} in roman numerals is {1}", arabicNum, romanNum);
-
-romanNum = "MMMCMXCIX";
-arabicNum = RomanNumeralConverter.RomanToArabic(romanNum);
-Console.WriteLine("{0} in arabic numerals is {1}", romanNum, arabicNum);
